6de9fa5f006433556c032113a52cdd728e80e420
Sagar Sharma <63660334+s-aga-r@users.noreply.github.com>
2021-08-10 18:16:50 +0530

    Fix error in test_transactions.py

--- tests/functional/test_transactions.py
+++ tests/functional/test_transactions.py
@@ -11,7 +11,7 @@ def test_index_page(client):
 
 
 # url - /transactions/issue-book
-def test_add_page(client):
+def test_issue_page(client):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/transactions/issue-book' page is requested (GET)
@@ -21,16 +21,3 @@ def test_add_page(client):
     assert response.status_code == 200
     assert b"Book Issue" in response.data
     assert b"Issue Date" in response.data
-
-
-# url - /transactions/return-book
-def test_edit_page(client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/transactions/return-book' page is requested (GET)
-    THEN check that the response is valid
-    """
-    response = client.get("/transactions/return-book")
-    assert response.status_code == 200
-    assert b"Book Return" in response.data
-    assert b"Return Date" in response.data
